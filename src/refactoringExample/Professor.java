fae7a14e2476f4589b05eface0f480200cae1b8d
1795516 <1795516@LI-S064-0T19>
2018-05-23 08:54:14 -0400

    class professor

--- src/refactoringExample/Professor.java
+++ src/refactoringExample/Professor.java
@@ -5,10 +5,24 @@
  */
 package refactoringExample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author 1795516
  */
 public class Professor {
+    List<Student> Students = new ArrayList<Student>();
+    String photoUrl;
     
+    public Professor(String lastName, String firstName){
+        String employeeID = lastName.toUpperCase().trim().substring(1, 3)
+                + firstName.toUpperCase().trim().substring(1, 3);
+        System.out.println("New professor: " + employeeID);
+    }
+    
+    public void setPhotoUrl(String photoName){
+        this.photoUrl = "/photos/professors/mad/" + photoName;
+    }
 }
